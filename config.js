8206dbede58d024b08f1668914c8fe88672dd2c7
Rian Tavares <rian.tavares@live.com>
2024-08-13 14:19:52 -0700

    set config for getting metadata from instructors by slug

--- config.js
+++ config.js
@@ -3,12 +3,8 @@ export const config = {
   patterns: [
       {
           pattern: "/instructor/[^/]+",
-          metaDataEndpoint: "https://xeo6-2sgh-ehgj.n7.xano.io/api:8wD10mRd/event/{id}/meta"
+          metaDataEndpoint: "https://alvuagzviphwrdjreldb.supabase.co/functions/v1/teacher-metadata?slug_input={slug_input}"
       }
-    //   {
-    //       pattern: "/team/profile/[^/]+",
-    //       metaDataEndpoint: "https://xeo6-2sgh-ehgj.n7.xano.io/api:LjwxezTv/team/profile/{profile_id}/meta"
-    //   }
       // Add more patterns and their metadata endpoints as needed
   ]
 };
